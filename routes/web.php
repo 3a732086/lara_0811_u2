4861f15a21c4e086643d39476e3a2e21e43c7c71
hu dong lin <benabcd5779@gmail.com>
2020-10-19 16:21:43 +0800

    設定Route回傳字串

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('r1',function(){
-    return redirect('r2');
+Route::get('/',function(){
+    return'welcome';
 });
 
-
-Route::get('r2', function () {
-    return view('welcome');
-});
 
 
