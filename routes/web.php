77455086c5de519aaddbef970612be92dbb01c62
hu dong lin <benabcd5779@gmail.com>
2020-10-19 16:33:40 +0800

    將 Route 取名為 hello.index

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('hello/{name?}',function($name='Everybody'){
-return'Hello,'.$name;
-});
-
-Route::get('/',function(){
-    return'welcome';
-});
+Route::get('hello/{name?}', function ($name='Everybody') {
+    return 'Hello, '.$name;
+})->name('hello.index');
