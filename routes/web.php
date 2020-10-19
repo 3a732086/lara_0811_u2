043cf826fe85981b633ffab688c8df98a9187bec
hu dong lin <benabcd5779@gmail.com>
2020-10-19 16:25:30 +0800

    修改 Route 接受參數

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('r1',function(){
-    return redirect('r2');
+Route::get('hello/{name}',function($name){
+    return'Hello,'.$name;
 });
 
-
-Route::get('r2', function () {
-    return view('welcome');
-});
 
 
