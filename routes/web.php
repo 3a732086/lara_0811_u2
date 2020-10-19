45bf973ca00ab9e6be3cbbce4dbff6df7c452c56
hu dong lin <benabcd5779@gmail.com>
2020-10-19 16:42:45 +0800

    修改 resources/views/welcome.blade.php的內容，增加三個連結

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,17 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/',function(){
+    return 'welcome';
+});
+
+Route::get('r1',function(){
+    return redirect('r2');
+});
+
+Route::get('r2',function(){
+    return view ('welcome');
+});
 
 Route::get('hello/{name?}', function ($name='Everybody') {
     return 'Hello, '.$name;
